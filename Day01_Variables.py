12bf8d8b902de059d1fb98e07f5868ea4762e2ab
Prachi Singh <prachi8128@gmail.com>
2026-07-08 22:09:48 +0530

    Create Day01_Variables.py
    Add Day 1 Python variables program

---
+++ Day01_Variables.py
@@ -0,0 +1,11 @@
+# Day 1 - Variables
+
+name = "Prachi Singh"
+country = "India"
+dream = "GKS Scholar"
+course = "MCA"
+
+print("Name:", name)
+print("Country:", country)
+print("Dream:", dream)
+print("Course:", course)
