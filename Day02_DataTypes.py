905ac635c70049c993466ba93888fdf2fd5eb1ca
Prachi Singh <prachi8128@gmail.com>
2026-07-08 22:13:43 +0530

    Create Day02_DataTypes.py

---
+++ Day02_DataTypes.py
@@ -0,0 +1,29 @@
+# -----------------------------------
+# Day 2 - Data Types
+# Author: Prachi Singh
+# -----------------------------------
+
+# String
+name = "Prachi"
+
+# Integer
+age = 22
+
+# Float
+cgpa = 7.91
+
+# Boolean
+gks_goal = True
+
+print("Name:", name)
+print("Age:", age)
+print("CGPA:", cgpa)
+print("GKS Goal:", gks_goal)
+
+print()
+
+# Check Data Types
+print(type(name))
+print(type(age))
+print(type(cgpa))
+print(type(gks_goal))
